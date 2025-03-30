0bb5a29702e31896623ef65189743ac6b554238a
timGalk <tivron@outlook.com>
2025-03-30 14:13:48 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/edu/cocktail_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.edu.cocktail_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Cocktail.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+class Cocktail {
+  final int id;
+  final String name;
+  final String category;
+  final String glass;
+  final String instructions;
+  final String imageUrl;
+  final bool alcoholic;
+
+  Cocktail({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.glass,
+    required this.instructions,
+    required this.imageUrl,
+    required this.alcoholic,
+  });
+
+  factory Cocktail.fromJson(Map<String, dynamic> json) {
+    return Cocktail(
+      id: json['id'],
+      name: json['name'],
+      category: json['category'],
+      glass: json['glass'],
+      instructions: json['instructions'],
+      imageUrl: json['image_url'],
+      alcoholic: json['alcoholic'],
+    );
+  }
+
+  static List<Cocktail> fromJsonList(String str) {
+    final List<dynamic> jsonData = json.decode(str);
+    return jsonData.map((e) => Cocktail.fromJson(e)).toList();
+  }
+}
